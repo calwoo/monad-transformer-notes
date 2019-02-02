357bc58c34992a11c931dfa4171e30eb13649cad
Calvin D. Woo <calvin.d.woo@gmail.com>
2019-02-01 21:33:26 -0500

    one MT, how about many?

--- transformers-mmh.hs
+++ transformers-mmh.hs
@@ -26,6 +26,53 @@ instance (Monad m) => Applicative (MaybeT m) where
         (runMaybeT x)
 
 -- so MaybeT is a monad!
+-- We can use this monad in situations where we combine IO and Maybe
 
+-- examples)
+readUserName :: MaybeT IO String
+readUserName = MaybeT $ do
+    str <- getLine
+    if length str > 5
+        then return $ Just str
+        else return Nothing
 
+readPassword :: MaybeT IO String
+readPassword = MaybeT $ do
+    pwd <- getLine
+    if length pwd < 6
+        then return Nothing
+        else return $ Just pwd
 
+main :: IO ()
+main = do
+    maybeCreds <- runMaybeT $ do
+        usr <- readUserName
+        pwd <- readPassword
+        return (usr, pwd)
+    case maybeCreds of
+        Nothing -> putStrLn "Couldn't login!"
+        Just (u, p) -> putStrLn "Verifying..."
+
+-- what does this code look like without the monad transformer?
+
+{- main :: IO ()
+main = do
+    maybeUsername <- readUserName
+    case maybeUsername of
+        Nothing -> putStrLn "Invalid name!"
+        Just uName -> do
+            maybePassword <- readPassword
+            case maybePassword of
+                Nothing -> putStrLn "Invalid pass!"
+                Just uPass -> putStrLn "Verifying..."
+ -}
+
+-- pretty bad.
+
+-- now as MaybeT is again a monad, we can wrap it in another monad! Hence we can get stacks of monad
+-- transformers...
+
+type Env = (Maybe String, Maybe String, Maybe String)
+
+readUserName' :: MaybeT (ReaderT Env IO) String
+readUserName'
